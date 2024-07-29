a6e75d9ef9af23ba480d2099fd208146876fa80c
fizlrock <nirku0soft@yandex.ru>
2024-07-29 17:56:29 +0400

    hibernate search by uuid test

--- code/src/test/java/dev/fizlrock/ears/EarsApplicationTests.java
+++ code/src/test/java/dev/fizlrock/ears/EarsApplicationTests.java
@@ -2,6 +2,8 @@ package dev.fizlrock.ears;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -14,6 +16,8 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import dev.fizlrock.ears.domain.entities.AudioRecordInfo;
+import dev.fizlrock.ears.domain.entities.User;
 import dev.fizlrock.ears.repository.AudioRecordInfoRepository;
 import dev.fizlrock.ears.repository.UserRepository;
 
@@ -38,28 +42,24 @@ class EarsApplicationTests {
 	@Autowired
 	UserRepository userRepo;
 
+
 	@Test
-	void fuckIt() {
-		assertTrue(true);
+	void testFindByUUID() {
+		var user = User.builder()
+				.username("fuck you")
+				.passwordHash("fuck it")
+				.build();
+		userRepo.save(user);
+		var audio = AudioRecordInfo.builder()
+				.id(UUID.randomUUID())
+				.owner(user)
+				.build();
+		audioRepo.save(audio);
+
+		var finded = audioRepo.findById(UUID.fromString(audio.getId().toString()));
+
+		assertTrue(finded.isPresent());
+
 	}
-
-	// @Test
-	// void testFindByUUID() {
-	// var user = User.builder()
-	// .username("fuck you")
-	// .passwordHash("fuck it")
-	// .build();
-	// userRepo.save(user);
-	// var audio = AudioRecordInfo.builder()
-	// .id(UUID.randomUUID())
-	// .owner(user)
-	// .build();
-	// audioRepo.save(audio);
-
-	// var finded = audioRepo.findById(UUID.fromString(audio.getId().toString()));
-
-	// assertTrue(finded.isPresent());
-
-	// }
 
 }
